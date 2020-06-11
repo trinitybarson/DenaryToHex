fc3746055e231c09d92e23d28a60f1909aa9ab73
trinitybarson <trinny.barson@gmail.com>
2020-06-11 22:21:01 +0100

    Add project files.

---
+++ DenaryToHex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DenaryToHex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Enter a denary number:");
+            try
+            {
+                // read denary number
+                number = int.Parse(Console.ReadLine());
+                // convert to hex
+                string hexNumber = number.ToString("X");
+                // output hex number
+                Console.WriteLine(number + " in hexidecimal is " + hexNumber);
+            }
+            catch
+            {
+                // if the user input is the wrong format
+                Console.WriteLine("Incorrect value entered, please enter a denary integer");
+            }
+            
+        }
+    }
+}
